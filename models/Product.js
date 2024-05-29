13689f1ad1bd3eb4df82f880ce69b691a544e914
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-05-28 22:35:02 -0500

    added table model relationships to models/index.js file

--- models/Product.js
+++ models/Product.js
@@ -16,7 +16,7 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    productName: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -30,7 +30,7 @@ Product.init(
       defaultValue: 10,
       isNumeric: true,
     },
-    categoryId: {
+    category_id: {
       type: DataTypes.INTEGER,
       references: {
         model: 'category',
