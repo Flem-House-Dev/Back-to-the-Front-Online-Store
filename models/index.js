d5ef6299c9290f22107a31c1c5a69edfa35f33d1
JFleming963 <144638343+JFleming963@users.noreply.github.com>
2024-05-29 08:36:40 -0500

    added api routes for categories

--- models/index.js
+++ models/index.js
@@ -14,7 +14,7 @@ Category.hasMany(Product, {
   foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
-ProductTag.belongsToMany(Tag, {
+Product.belongsToMany(Tag, {
   through: ProductTag
 });
 
